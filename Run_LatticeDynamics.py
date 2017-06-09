0cd413ecdb2baccf77aede67def4cb01c9f0f91a
Nate <nathan.abraham@colorado.edu>
2017-06-09 13:26:27 -0600

    Updated to read the input file

--- Run_LatticeDynamics.py
+++ Run_LatticeDynamics.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import numpy as np
+import subprocess
 import ThermodynamicProperties as Pr
 import Wavenumbers as Wvn
 import Thermal_NumericalAnalysis as TNA
@@ -82,78 +83,240 @@ def Lattice_Dynamics(Temperature=[0.0, 25.0, 50.0, 75.0, 100.0], Pressure=1, Met
         print "Gradient Anisotropic Quasi-Harmonic Approximation is complete!"
 
 
+
+
 if __name__ == '__main__':
     
     import argparse 
     
     parser = argparse.ArgumentParser(description='Calculate free energies as a function of T using lattice dynamics')
     
-    parser.add_argument('-a', '--temperature', dest='Temperature', nargs='+', type=float,
-                        default=[0.0, 25.0, 50.0, 75.0, 100.0], help='Array of temperatures [K]')
-    parser.add_argument('-b', '--pressure', dest='Pressure', default='1', help='Pressure (atm)')
-    parser.add_argument('-c', '--method', dest='Method', default='HA', help='type of lattice dynamics to perform',
-                        choices=['HA', 'SiQ', 'SiQg', 'GiQ', 'GiQg', 'GaQ', 'GaQg'])
-    parser.add_argument('-d', '--output', dest='Output', default='out', help='Output string')
-    parser.add_argument('-e', '--program', dest='Program', default='Tinker',
-                        help='method to use to calculate wavenumbers', choices=['Tinker', 'Test'])
-    parser.add_argument('-g', '--coordfile', dest='Coordinate_file', default='molecule.xyz', type=file,
-                        help='coordinate file for crystal (in the format the program accepts)')
-    parser.add_argument('-i', '--parameter file', dest='Parameter_file', default='keyfile.key', type=file,
-                        help='parameter file for crystal (in the format the program accepts)')
-    parser.add_argument('-j', '--molecules_in_coord', dest='molecules_in_coord', default=4, type=int,
-                        help='number of molecules in the coordinate file')
-    parser.add_argument('-k', '--properties', dest='properties_to_save', nargs='+', default=['G', 'T'],
-                        help='calculated properties to store')
-    parser.add_argument('-l', '--integration_method', dest='NumAnalysis_method', default='RK4', choices=['RK4'],
-                        help='choice of method for temperature gradient integration')
-    parser.add_argument('-m', '--integration_step', dest='NumAnalysis_step', default=25.0, type=float,
-                        help='step size for temperature gradient integration')
-    #MRS: seems like volume and box lattice fraction parameters should be combined?
-    parser.add_argument('-o', '--gradient_vdelta', dest='LocGrd_Vol_FracStep', default=3e-02, type=float,
-                        help='step size fnumerical calculation of gradient in volume fraction (in fraction)')
-    parser.add_argument('-p', '--gradient_boxdelta', dest='LocGrd_LatParam_FracStep', default=5e-05, type=float,
-                        help='steps size for box lattice parameter fraction (in fraction)')
-    parser.add_argument('-q', '--step_volfrac', dest='StepWise_Vol_StepFrac', default=1.5e-3, type=float,
-                        help='step size for stepwise volume change')
-    parser.add_argument('-r', '--step_volmin', dest='StepWise_Vol_LowerFrac', default=0.97, type=float,
-                        help='minimum relative change for stepwise volume change')
-    parser.add_argument('-s', '--step_volmax', dest='StepWise_Vol_UpperFrac', default=1.16, type=float,
-                        help='maximum relative change for stepwise volume change' )
-    parser.add_argument('-t', '--statistics', dest='Statistical_Mechanics', default='Classical',
-                        choices=['Quantum', 'Classical'],
-                        help='type of statistics to use for calculating the lattice dynamics')
-    parser.add_argument('-u', '--gruneisen_vol_step', dest='Gruneisen_Vol_FracStep', type=float, default=1.5e-3,
-                        help='Gruneisen volume fraction change')
-    parser.add_argument('-v', '--wavenum_tol', dest='Wavenum_Tol', default=-1.0, type=float,
-                        help='minimum negative wavenumber accepted (negative wavenumbers ignored)')
-    parser.add_argument('-w', '--maxtemp', dest='Gradient_MaxTemp', default=300.0, type=float,
-                        help='maximum temperature change for gradient approach')
-    parser.add_argument('-x', '--anisotropy', dest='Aniso_LocGrad_Type', choices=['73'], default='73',
-                        help='anisotropy approximation used')
-    parser.add_argument('-y', '--order', dest='Gruneisen_order', choices=['First','Second'], default='First',
-                        help='Order of Gruneisen parameter')
+    parser.add_argument('-i', '--input_file', dest='Input_file', default='input_test.py', #type=file,
+                        help='Input file containing all parameters for the run')
 
     args = parser.parse_args()
 
-    Lattice_Dynamics(Temperature=args.Temperature,
-                     Pressure=args.Pressure,
-                     Method=args.Method,
-                     Program=args.Program,
-                     Output=args.Output,
-                     Coordinate_file=args.Coordinate_file,
-                     Parameter_file=args.Parameter_file,
-                     molecules_in_coord=args.molecules_in_coord,
-                     properties_to_save=args.properties_to_save,
-                     NumAnalysis_method=args.NumAnalysis_method,
-                     NumAnalysis_step=args.NumAnalysis_step,
-                     LocGrd_Vol_FracStep=args.LocGrd_Vol_FracStep,
-                     LocGrd_LatParam_FracStep=args.LocGrd_LatParam_FracStep,
-                     StepWise_Vol_StepFrac=args.StepWise_Vol_StepFrac,
-                     StepWise_Vol_LowerFrac=args.StepWise_Vol_LowerFrac,
-                     StepWise_Vol_UpperFrac=args.StepWise_Vol_UpperFrac,
-                     Statistical_mechanics=args.Statistical_mechanics,
-                     Gruneisen_Vol_FracStep=args.options.Gruneisen_Vol_FracStep,
-                     Wavenum_Tol=args.Wavenum_Tol,
-                     Gradient_MaxTemp=args.Gradient_MaxTemp,
-                     Aniso_LocGrad_Type=args.Aniso_LocGrad_Type,
-                     Gruneisen_order=args.Gruneisen_order)
+    try:
+        Method = subprocess.check_output("less " + str(args.Input_file) + " | grep Method | grep = ", shell=True)
+        Method = Method.split('=')[1].strip()
+        if any(Method == np.array(['HA', 'SiQ', 'SiQg', 'GiQ', 'GiQg', 'GaQ', 'GaQg'])) == False:
+            print "Input method is not supported. Please select from the following:"
+            print "   HA, SiQ, SiQg, GiQ, GiQg, GaQ, GaQg"
+            print "Exiting code"
+            sys.exit()
+    except subprocess.CalledProcessError as grepexc:
+        print "No method was selected"
+        print "Exiting code"
+        sys.exit()
+
+    try:
+        Program = subprocess.check_output("less " + str(args.Input_file) + " | grep Program | grep = ", shell=True)
+        Program = Program.split('=')[1].strip()
+        if any(Program == np.array(['Tinker', 'Test'])) == False:
+            print "Input program is not supported. Please select from the following:"
+            print "   Tinker, Test"
+            print "Exiting code"
+            sys.exit()
+    except subprocess.CalledProcessError as grepexc:
+        print "No program was selected"
+        print "Exiting code"
+        sys.exit()
+
+    try:
+        Statistical_mechanics = subprocess.check_output("less " + str(args.Input_file) + " | grep Statistical_mechanics"
+                                                                                         " | grep = ", shell=True)
+        Statistical_mechanics = Statistical_mechanics.split('=')[1].strip()
+        if any(Statistical_mechanics == np.array(['Classical', 'Quantum'])) == False:
+            print "Input statistical mechanics is not supported. Please select from the following:"
+            print "   Classical, Quantum"
+            print "Exiting code"
+            sys.exit()
+    except subprocess.CalledProcessError as grepexc:
+        print "Statistical mechnics was not specified"
+        print "Exiting code"
+        sys.exit()
+
+    try:
+        Temperature = subprocess.check_output("less " + str(args.Input_file) + " | grep Temperature"
+                                                                               " | grep = ", shell=True)
+        Temperature = np.array(Temperature.split('=')[1].strip().split(',')).astype(float)
+    except subprocess.CalledProcessError as grepexc:
+        Temperature = [0.0, 25.0, 50.0, 75.0, 100.0]
+        if any(Method == np.array(['HA', 'SiQ', 'SiQg'])):
+            print "No temperatures were selected, using default temperatures of:"
+            print Temperature
+
+    try:
+        Pressure = subprocess.check_output("less " + str(args.Input_file) + " | grep Pressure | grep = ", shell=True)
+        Pressure = float(Pressure.split('=')[1].strip())
+    except subprocess.CalledProcessError as grepexc:
+        print "No pressure was selected, using default pressure"
+        Pressure = 1.
+
+    try:
+        Output = subprocess.check_output("less " + str(args.Input_file) + " | grep Output | grep = ", shell=True)
+        Output = Output.split('=')[1].strip()
+    except subprocess.CalledProcessError as grepexc:
+        Output = 'out'
+
+    try:
+        Coordinate_file = subprocess.check_output("less " + str(args.Input_file) + " | grep Coordinate_file"
+                                                                                   " | grep = ", shell=True)
+        Coordinate_file = Coordinate_file.split('=')[1].strip()
+    except subprocess.CalledProcessError as grepexc:
+        print "Coordinate file was not provided"
+        print "Exiting code"
+        sys.exit()
+
+    try:
+        Parameter_file = subprocess.check_output("less " + str(args.Input_file) + " | grep Parameter_file  | grep = ", shell=True)
+        Parameter_file = Parameter_file.split('=')[1].strip()
+    except subprocess.CalledProcessError as grepexc:
+        Parameter_file = ''
+        if Program == 'Tinker':
+            print "Parameter file was not provided for Tinker"
+            print "Exiting code"
+            sys.exit()
+
+    try:
+        molecules_in_coord = subprocess.check_output("less " + str(args.Input_file) + " | grep molecules_in_coord"
+                                                                                      " | grep = ", shell=True)
+        molecules_in_coord = float(molecules_in_coord.split('=')[1].strip())
+    except subprocess.CalledProcessError as grepexc:
+        if Program == 'Test':
+            molecules_in_coord = 1
+        else:
+            print "Number of molecules in system was not specified"
+            print "Exiting code"
+            sys.exit()
+
+    try:
+        properties_to_save_temp = subprocess.check_output("less " + str(args.Input_file) + " | grep properties_to_save"
+                                                                                      " | grep = ", shell=True)
+        properties_to_save_temp = np.array(properties_to_save_temp.split('=')[1].strip().split(','))
+        properties_to_save = []
+        for i in range(len(properties_to_save_temp)):
+            if any(properties_to_save_temp[i] == np.array(['G', 'S', 'T', 'P', 'Av', 'V', 'h', 'U'])):
+                properties_to_save.append(properties_to_save_temp[i])
+            else:
+                print "The following input is not a choice in properites: " + properties_to_save_temp[i]
+        properties_to_save = np.array(properties_to_save)
+    except subprocess.CalledProcessError as grepexc:
+        properties_to_save = np.array(['G', 'T'])
+
+    try:
+        NumAnalysis_method = subprocess.check_output("less " + str(args.Input_file) + " | grep NumAnalysis_method"
+                                                                                      " | grep = ", shell=True)
+        NumAnalysis_method = NumAnalysis_method.split('=')[1].strip()
+    except subprocess.CalledProcessError as grepexc:
+        NumAnalysis_method = 'Euler'
+        if any(Method == np.array(['GiQ', 'GiQg', 'GaQ', 'GaQg'])):
+            print "Numerical analysis method  was not specified"
+            print "... Using default method: Euler"
+
+    try:
+        NumAnalysis_step = subprocess.check_output("less " + str(args.Input_file) + " | grep NumAnalysis_step"
+                                                                                    " | grep = ", shell=True)
+        NumAnalysis_step = NumAnalysis_step.split('=')[1].strip()
+    except subprocess.CalledProcessError as grepexc:
+        NumAnalysis_step = 150.
+        if any(Method == np.array(['GiQ', 'GiQg', 'GaQ', 'GaQg'])):
+            print "Numerical analysis step size  was not specified"
+            print "... Using default step size: " + str(NumAnalysis_step)
+
+    try:
+        LocGrd_Vol_FracStep = subprocess.check_output("less " + str(args.Input_file) + " | grep LocGrd_Vol_FracStep"
+                                                                                       " | grep = ", shell=True)
+        LocGrd_Vol_FracStep = LocGrd_Vol_FracStep.split('=')[1].strip()
+    except subprocess.CalledProcessError as grepexc:
+        LocGrd_Vol_FracStep = 3e-02
+
+    try:
+        LocGrd_LatParam_FracStep = subprocess.check_output("less " + str(args.Input_file) + " | grep "
+                                                                                            "LocGrd_LatParam_FracStep"
+                                                                                            " | grep = ", shell=True)
+        LocGrd_LatParam_FracStep = LocGrd_LatParam_FracStep.split('=')[1].strip()
+    except subprocess.CalledProcessError as grepexc:
+        LocGrd_LatParam_FracStep = 5e-02
+
+    try:
+        StepWise_Vol_StepFrac = subprocess.check_output("less " + str(args.Input_file) + " | grep StepWise_Vol_StepFrac"
+                                                                                         " | grep = ", shell=True)
+        StepWise_Vol_StepFrac = StepWise_Vol_StepFrac.split('=')[1].strip()
+    except subprocess.CalledProcessError as grepexc:
+        StepWise_Vol_StepFrac = 1.5e-03
+
+    try:
+        StepWise_Vol_LowerFrac = subprocess.check_output("less " + str(args.Input_file) + " | grep "
+                                                                                          "StepWise_Vol_LowerFrac"
+                                                                                          " | grep = ", shell=True)
+        StepWise_Vol_LowerFrac = StepWise_Vol_LowerFrac.split('=')[1].strip()
+    except subprocess.CalledProcessError as grepexc:
+        StepWise_Vol_LowerFrac = 0.99
+
+    try:
+        StepWise_Vol_UpperFrac = subprocess.check_output("less " + str(args.Input_file) + " | grep "
+                                                                                          "StepWise_Vol_UpperFrac"
+                                                                                          " | grep = ", shell=True)
+        StepWise_Vol_UpperFrac = StepWise_Vol_UpperFrac.split('=')[1].strip()
+    except subprocess.CalledProcessError as grepexc:
+        StepWise_Vol_UpperFrac = 1.02
+
+    try:
+        Gruneisen_Vol_FracStep = subprocess.check_output("less " + str(args.Input_file) + " | grep "
+                                                                                          "Gruneisen_Vol_FracStep"
+                                                                                          " | grep = ", shell=True)
+        Gruneisen_Vol_FracStep = Gruneisen_Vol_FracStep.split('=')[1].strip()
+    except subprocess.CalledProcessError as grepexc:
+        Gruneisen_Vol_FracStep = 1.5e-03
+
+    try:
+        Wavenum_Tol = subprocess.check_output("less " + str(args.Input_file) + " | grep Wavenum_Tol"
+                                                                               " | grep = ", shell=True)
+        Wavenum_Tol = float(Wavenum_Tol.split('=')[1].strip())
+    except subprocess.CalledProcessError as grepexc:
+        Wavenum_Tol = -1.
+
+    try:
+        Gradient_MaxTemp = subprocess.check_output("less " + str(args.Input_file) + " | grep Gradient_MaxTemp"
+                                                                                    " | grep = ", shell=True)
+        Gradient_MaxTemp = Gradient_MaxTemp.split('=')[1].strip()
+    except subprocess.CalledProcessError as grepexc:
+        Gradient_MaxTemp = 300.
+
+    try:
+        Aniso_LocGrad_Type = subprocess.check_output("less " + str(args.Input_file) + " | grep Aniso_LocGrad_Type"
+                                                                                      " | grep = ", shell=True)
+        Aniso_LocGrad_Type = Aniso_LocGrad_Type.split('=')[1].strip()
+    except subprocess.CalledProcessError as grepexc:
+        Aniso_LocGrad_Type = 73
+
+#    try:
+#        Gruneisen_order = subprocess.check_output("less " + str(args.Input_file) + " | grep Gruneisen_order"
+#                                                                                   " | grep = ", shell=True)
+#        Gruneisen_order = Gruneisen_order.split('=')[1].strip()
+#    except subprocess.CalledProcessError as grepexc:
+#        Gruneisen_order = 'First'
+
+    Lattice_Dynamics(Temperature=Temperature,
+                     Pressure=Pressure,
+                     Method=Method,
+                     Program=Program,
+                     Output=Output,
+                     Coordinate_file=Coordinate_file,
+                     Parameter_file=Parameter_file,
+                     molecules_in_coord=molecules_in_coord,
+                     properties_to_save=properties_to_save,
+                     NumAnalysis_method=NumAnalysis_method,
+                     NumAnalysis_step=NumAnalysis_step,
+                     LocGrd_Vol_FracStep=LocGrd_Vol_FracStep,
+                     LocGrd_LatParam_FracStep=LocGrd_LatParam_FracStep,
+                     StepWise_Vol_StepFrac=StepWise_Vol_StepFrac,
+                     StepWise_Vol_LowerFrac=StepWise_Vol_LowerFrac,
+                     StepWise_Vol_UpperFrac=StepWise_Vol_UpperFrac,
+                     Statistical_mechanics=Statistical_mechanics,
+                     Gruneisen_Vol_FracStep=Gruneisen_Vol_FracStep,
+                     Wavenum_Tol=Wavenum_Tol,
+                     Gradient_MaxTemp=Gradient_MaxTemp,
+                     Aniso_LocGrad_Type=Aniso_LocGrad_Type)
+#                     Gruneisen_order=args.Gruneisen_order)
